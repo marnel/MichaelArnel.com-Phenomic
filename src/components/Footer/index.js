c8b025fdfabb5f9d3668431ff3e7ceb1ec082117
Michael Arnel <michael.arnel@gmail.com>
2017-05-17 21:52:49 -0400

    add footer, font-awesome other misc css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,42 @@
 import React from "react"
+import { Grid, Row, Col } from "react-bootstrap"
 
 import styles from "./index.css"
 
 const Footer = () => (
-  <footer className={ styles.footer }>
-    { /* If you like Phenomic, this is a way to share the love ;) */ }
-    <p>
-      <a
-        href={ process.env.PHENOMIC_HOMEPAGE }
-        className={ styles.phenomicReference }
-      >
-        { "Website generated with (MIKE) " }
-        <span className={ styles.phenomicReferenceName }>
-          {  `<${ process.env.PHENOMIC_NAME} />` }
-        </span>
-      </a>
-    </p>
-  </footer>
+  <footer id="footer" className={ styles.footer }>
+    <div className="container">
+      <Grid>
+        <Row className="show-grid">
+          <Col xs={12} md={8}> <p className={ styles.copyright }>Copyright &copy; <span>2017</span> Michael Arnel</p></Col>
+          <Col xs={6} md={4}>
+            <ul className="social">
+                <li>
+                    <a href="http://wwww.fb.com/" className="fa fa-facebook">
+                        <i className="ion-social-facebook"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://wwww.twitter.com/michaelarnel" className="fa fa-twitter">
+                        <i className="ion-social-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#" className="fa fa-linkedin">
+                        <i className="ion-social-linkedin"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://wwww.fb.com" className="fa fa-google-plus">
+                        <i className="ion-social-googleplus"></i>
+                    </a>
+                </li>
+            </ul>
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  </footer> 
 )
 
 export default Footer
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,40 +9,7 @@ import gitHubSvg from "../icons/iconmonstr-github-1.svg"
 import styles from "./index.css"
 
 const Header = (props, { metadata: { pkg } }) => (
- /* <header className={ styles.header }>
-    <nav className={ styles.nav }>
-      <div className={ styles.navPart1 }>
-        <Link
-          className={ styles.link }
-          to={ "/" }
-        >
-          { "Home" }
-        </Link>
-      </div>
-      <div className={ styles.navPart2 }>
-        {
-          pkg.twitter &&
-          <a
-            href={ `https://twitter.com/${pkg.twitter}` }
-            className={ styles.link }
-          >
-            <Svg svg={ twitterSvg } cleanup />
-            { "Twitter" }
-          </a>
-        }
-        {
-          pkg.repository &&
-          <a
-            href={ pkg.repository }
-            className={ styles.link }
-          >
-            <Svg svg={ gitHubSvg } cleanup />
-            { "GitHub" }
-          </a>
-        }
-      </div>
-    </nav>
-  </header> */
+ 
   <Navbar collapseOnSelect className={ styles.animatedHeader}>
     <Navbar.Header>
       <Navbar.Brand className={ styles.brandLogo } >
