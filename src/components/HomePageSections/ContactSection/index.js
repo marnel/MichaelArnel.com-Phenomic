25d859d13b6611f3dddc87a256f46586fa9a3b16
Michael Arnel <michael.arnel@gmail.com>
2017-05-22 19:01:04 -0400

    update contact component

--- src/components/HomePageSections/ContactSection/index.js
+++ src/components/HomePageSections/ContactSection/index.js
@@ -8,8 +8,8 @@ const ContactSection = (props) => {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="block">
-                            <h2 className="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">SO WHAT YOU THINK ?</h2>
-                            <p className="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis,<br />possimus commodi, fugiat magnam temporibus vero magni recusandae? Dolore, maxime praesentium.</p>
+                            <h2 className="title wow fadeInDown" data-wow-delay=".3s" data-wow-duration="500ms">Need to contact me?</h2>
+                            <p className="wow fadeInDown" data-wow-delay=".5s" data-wow-duration="500ms">Need help with a project, or have a technical question<br /> you can't seem to find the answers for? Hit the contact button below to reach out to me!</p>
                             <a href="contact.html" className="btn btn-default btn-contact wow fadeInDown" data-wow-delay=".7s" data-wow-duration="500ms">Contact Me</a>
                         </div>
                     </div> 
